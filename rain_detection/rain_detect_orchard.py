fdc1c47e831daa9469269f409630248f54b29816
Ekapop <eploentham@gmail.com>
2026-07-01 13:15:53 +0700

    Update rain_detect_orchard.py

--- rain_detection/rain_detect_orchard.py
+++ rain_detection/rain_detect_orchard.py
@@ -9,7 +9,7 @@ Rain Detection for Orchard - WITH DATABASE LOGGING
 - Designed for Raspberry Pi (SD card friendly)
 - Headless (no display window) - run as a systemd service
 """
-
+#update: 2026-07-01 user database name to smartfarm, and user to ekapop, password to Ekartc2c51*
 import cv2
 import numpy as np
 import time
@@ -29,7 +29,7 @@ RTSP_URL = "rtsp://USERNAME:PASSWORD@192.168.0.252:554/stream1"
 SENSOR_DEVICE = "nw01_orchard01_camera_01"
 
 # Database - same credentials as mqtt.py
-DB_CONFIG = {    'host': 'localhost',    'user': 'ekapop',    'password': 'Ekartc2c51*',    'database': 'smartfarm'}
+DB_CONFIG = {    'host': '192.168.0.253',    'user': 'ekapop',    'password': 'Ekartc2c51*',    'database': 'smartfarm'}
 
 # Check interval in seconds
 CHECK_INTERVAL = 30
